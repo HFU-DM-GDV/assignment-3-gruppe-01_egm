3ef43ac8a995c0a7e82ca9828f19c66a150a2eaf
GuidoGruen <101643865+GuidoGruen@users.noreply.github.com>
2023-05-24 11:24:09 +0200

    nordwesten_suedwesten

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import cv2
 from image_transform import guided_image_transform 
 from Hybrid_imaging import create_hybrid_image
 
-img_1 = cv2.imread("data/images/angry_man.png", cv2.IMREAD_COLOR)
+img_1 = cv2.imread("data/images/Nordwesten.png", cv2.IMREAD_COLOR)
 
-img_2 = cv2.imread("data/images/woman.png", cv2.IMREAD_COLOR)
+img_2 = cv2.imread("data/images/Suedosten.png", cv2.IMREAD_COLOR)
 
 img_2_transformed = guided_image_transform(img_1, img_2)
 
-create_hybrid_image(img_1, img_2_transformed, 7, 13)
+create_hybrid_image(img_1, img_2_transformed, 7, 20)
