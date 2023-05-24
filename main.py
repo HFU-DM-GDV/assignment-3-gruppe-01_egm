8f749a3fd928fb120460402c1767607e8d818a67
GuidoGruen <101643865+GuidoGruen@users.noreply.github.com>
2023-05-24 10:55:23 +0200

    Angry_man_woman_final

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 import numpy as np
 import cv2
 from image_transform import guided_image_transform 
-from hybrid_imaging import create_hybrid_image
+from Hybrid_imaging import create_hybrid_image
 
-img_1 = cv2.imread("data/images/tiger stare.jpg", cv2.IMREAD_COLOR)
+img_1 = cv2.imread("data/images/angry_man.png", cv2.IMREAD_COLOR)
 
-img_2 = cv2.imread("data/images/jaguar stare.jpg", cv2.IMREAD_COLOR)
+img_2 = cv2.imread("data/images/woman.png", cv2.IMREAD_COLOR)
 
 img_2_transformed = guided_image_transform(img_1, img_2)
 
-create_hybrid_image(img_1, img_2_transformed, 3, 20)
-
+create_hybrid_image(img_1, img_2_transformed, 7, 13)
