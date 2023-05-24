1960f389de5d92acb04c163c9269999e7dc3b431
tiram-isu <93144973+tiram-isu@users.noreply.github.com>
2023-05-24 11:32:17 +0200

    added dog+jaguar result

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import cv2
 from image_transform import guided_image_transform 
 from hybrid_imaging import create_hybrid_image
 
-img_1 = cv2.imread("data/images/Nordwesten.png", cv2.IMREAD_COLOR)
+img_1 = cv2.imread("data/images/jaguar stare.jpg", cv2.IMREAD_COLOR)
 
-img_2 = cv2.imread("data/images/Suedosten.png", cv2.IMREAD_COLOR)
+img_2 = cv2.imread("data/images/dog stare.jpg", cv2.IMREAD_COLOR)
 
 img_2_transformed = guided_image_transform(img_1, img_2)
 
-create_hybrid_image(img_1, img_2_transformed, 3, 20)
+create_hybrid_image(img_1, img_2_transformed, 10, 13)
